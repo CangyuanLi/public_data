3c84bb6313d762449c073ada13c422b357e2f2b0
Cangyuan Li <everest229@gmail.com>
2024-05-14 13:07:03 -0400

    initial commit

---
+++ census/scripts/download_census.r
@@ -0,0 +1,151 @@
+library(arrow)
+library(dplyr)
+library(here)
+library(readr)
+library(stringr)
+library(tidycensus)
+library(tidyr)
+
+tidycensus::census_api_key("4067c86fb24937a0063e3053a90602f2f32ad461", install = TRUE, overwrite = TRUE)
+readRenviron("~/.Renviron")
+
+BASE_PATH <- here::here()
+
+vars <- tidycensus::load_variables(year = 2019, dataset = "acs5")
+vars <- vars %>%
+    dplyr::filter(stringr::str_detect(concept, "SEX"))
+
+# 
+vars <- tidycensus::load_variables(year = 2019, dataset = "acs5")
+vars <- vars %>%
+  dplyr::filter(stringr::str_detect(name, "B01001"))
+# When getting data from the decennial census by "tract" or "block group", the state
+# must be specified
+
+ACS5_YEARS <- c(2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
+
+
+df_list <- list()
+
+for (year in ACS5_YEARS) {
+  df <- tidycensus::get_acs(
+    geography = "zcta",
+    table = "B01001",
+    year = year,
+    sumfile = "acs5",
+    cache_table = TRUE
+  ) %>%
+    dplyr::mutate(year = year) %>%
+    dplyr::select(-GEOID)
+  
+  df_list[[year]] <- df
+}
+
+combined <- dplyr::bind_rows(df_list)
+
+readr::write_csv(combined, file.path(BASE_PATH, "census/data/raw/acs5_sex_by_zcta5.csv"))
+
+
+get_race_by_zcta_acs5yr <- function() {
+  df_list <- list()
+  
+  for (year in ACS5_YEARS) {
+    df <- tidycensus::get_acs(
+      geography = "zcta",
+      table = "B03002",
+      year = year,
+      sumfile = "acs5",
+      cache_table = TRUE
+    ) %>%
+      dplyr::mutate(year = year) %>%
+      dplry::select(-GEOID)
+    
+    df_list[[year]] <- df
+  }
+  
+  combined <- dplyr::bind_rows(df_list)
+  
+  readr::write_csv(combined, file.path(BASE_PATH, "census/data/raw/race_by_zcta.csv"))
+}
+
+get_race_by_zcta_acs5yr()
+
+# clean_race <- function(df) {
+#     df %>%
+#         tidyr::pivot_wider(
+#             id_cols = GEOID, names_from = variable, values_from = value
+#         ) %>%
+#         dplyr::rename(
+#             total = P11_001N,
+#             hispanic = P11_002N,
+#             nh_total = P11_003N,
+#             nh_white = P11_005N,
+#             nh_black = P11_006N,
+#             nh_aian = P11_007N,
+#             nh_asian = P11_008N,
+#             nh_hpi = P11_009N,
+#             nh_other = P11_010N,
+#             nh_multiracial = P11_011N,
+#             nh_white_other = P11_017N,
+#             nh_black_other = P11_021N,
+#             nh_aian_other = P11_024N,
+#             nh_asian_other = P11_026N,
+#             nh_hpi_other = P11_027N,
+#             nh_asian_hpi_other = P11_048N,
+#         ) %>%
+#         dplyr::select(
+#             GEOID, total, hispanic, nh_total,
+#             nh_white, nh_black, nh_aian, nh_hpi, nh_other,
+#             nh_multiracial, nh_white_other, nh_black_other,
+#             nh_aian_other, nh_asian_other, nh_hpi_other,
+#             nh_asian_hpi_other
+#         )
+# }
+# 
+# get_race_by_zcta <- function() {
+#     df <- tidycensus::get_decennial(
+#         geography = "zcta",
+#         table = "P11",
+#         year = 2020,
+#         sumfile = "dhc",
+#         cache_table = TRUE
+#     )
+#     arrow::write_parquet(df, file.path(CENSUS_PATH, "zcta/raw/zcta.parquet"))
+# }
+
+/
+# get_race_by_tract()
+# get_race_by_block_group()
+
+
+
+# get_race_by_zcta <- function(year) {
+#     race_by_zcta <- tidycensus::get_acs(geography = "zcta", table = "B03002", year = year, cache_table = TRUE) %>%
+#         tidyr::pivot_wider(id_cols = NAME, names_from = variable, values_from = estimate) %>%
+#         dplyr::rename(
+#             zcta = NAME,
+#             Total_Pop = B03002_001,
+#             Non_Hispanic_Total = B03002_002,
+#             NH_White_alone = B03002_003,
+#             NH_AIAN_alone = B03002_005,
+#             NH_Black_alone = B03002_004,
+#             nh_asian = B03002_006,
+#             nh_hawaiian = B03002_007,
+#             other = B03002_008,
+#             multiple = B03002_009,
+#             Hispanic_Total = B03002_012,
+#         ) %>%
+#         dplyr::mutate(
+#             zcta = zcta %>%
+#                 stringr::str_remove("ZCTA5") %>%
+#                 stringr::str_trim(),
+#             year = year,
+#             nh_api = nh_asian + nh_hawaiian,
+#         ) %>%
+#         dplyr::select(zcta, nh_white, nh_black, nh_native, nh_api, hispanic, multiple)
+
+#     return(race_by_zcta)
+# }
+
+# race_by_zcta <- get_race_by_zcta(2021)
+# arrow::write_parquet(race_by_zcta, file.path(CENSUS_PATH, "acs5yr2021_race.parquet"))s
---
+++ census/scripts/process_race.py
@@ -0,0 +1,199 @@
+from pathlib import Path
+
+import polars as pl
+import polars.selectors as cs
+
+BASE_PATH = Path(__file__).resolve().parents[1]
+
+
+def main():
+    df = (
+        pl.scan_csv(BASE_PATH / "data/raw/acs5_sex_age_by_zcta5.csv", null_values="NA")
+        .drop("moe")
+        .rename({"NAME": "zcta5"})
+        .with_columns(pl.col("zcta5").str.replace_all("ZCTA5 ", "").cast(pl.Int64))
+        .collect()
+        .pivot(values="estimate", index=["zcta5", "year"], columns="variable")
+        .select(cs.all().name.map(lambda c: c.replace("B01001_", "")))
+        .rename(
+            {
+                "001": "total",
+                "002": "male",
+                "003": "male_l_5",
+                "004": "male_5_9",
+                "005": "male_10_14",
+                "006": "male_15_17",
+                "007": "male_18_19",
+                "008": "male_20",
+                "009": "male_21",
+                "010": "male_22_24",
+                "011": "male_25_29",
+                "012": "male_30_34",
+                "013": "male_35_39",
+                "014": "male_40_44",
+                "015": "male_45_49",
+                "016": "male_50_54",
+                "017": "male_55_59",
+                "018": "male_60_61",
+                "019": "male_62_64",
+                "020": "male_65_66",
+                "021": "male_67_69",
+                "022": "male_70_74",
+                "023": "male_75_79",
+                "024": "male_80_84",
+                "025": "male_ge_85",
+                "026": "female",
+                "027": "female_l_5",
+                "028": "female_5_9",
+                "029": "female_10_14",
+                "030": "female_15_17",
+                "031": "female_18_19",
+                "032": "female_20",
+                "033": "female_21",
+                "034": "female_22_24",
+                "035": "female_25_29",
+                "036": "female_30_34",
+                "037": "female_35_39",
+                "038": "female_40_44",
+                "039": "female_45_49",
+                "040": "female_50_54",
+                "041": "female_55_59",
+                "042": "female_60_61",
+                "043": "female_62_64",
+                "044": "female_65_66",
+                "045": "female_67_69",
+                "046": "female_70_74",
+                "047": "female_75_79",
+                "048": "female_80_84",
+                "049": "female_ge_85",
+            }
+        )
+        .to_pandas()
+    )
+
+    df["l_62"] = df[
+        [
+            "male_l_5",
+            "male_5_9",
+            "male_10_14",
+            "male_15_17",
+            "male_18_19",
+            "male_20",
+            "male_21",
+            "male_22_24",
+            "male_25_29",
+            "male_30_34",
+            "male_35_39",
+            "male_40_44",
+            "male_45_49",
+            "male_50_54",
+            "male_55_59",
+            "male_60_61",
+            "female_l_5",
+            "female_5_9",
+            "female_10_14",
+            "female_15_17",
+            "female_18_19",
+            "female_20",
+            "female_21",
+            "female_22_24",
+            "female_25_29",
+            "female_30_34",
+            "female_35_39",
+            "female_40_44",
+            "female_45_49",
+            "female_50_54",
+            "female_55_59",
+            "female_60_61",
+        ]
+    ].sum(axis=1)
+
+    df["ge_62"] = df[
+        [
+            "male_62_64",
+            "male_65_66",
+            "male_67_69",
+            "male_70_74",
+            "male_75_79",
+            "male_80_84",
+            "male_ge_85",
+            "female_62_64",
+            "female_65_66",
+            "female_67_69",
+            "female_70_74",
+            "female_75_79",
+            "female_80_84",
+            "female_ge_85",
+        ]
+    ].sum(axis=1)
+
+    df["18_62"] = df["l_62"] - df[
+        [
+            "male_l_5",
+            "male_5_9",
+            "male_10_14",
+            "male_15_17",
+            "female_l_5",
+            "female_5_9",
+            "female_10_14",
+            "female_15_17",
+        ]
+    ].sum(axis=1)
+
+    sex = df[["zcta5", "year", "male", "female"]]
+    age = df[["zcta5", "year", "l_62", "18_62", "ge_62"]]
+
+    sex.to_parquet(BASE_PATH / "data/sex_by_zcta5.parquet", index=False)
+    age.to_parquet(BASE_PATH / "data/age_by_zcta5.parquet", index=False)
+
+
+    df = (
+        pl.scan_csv(BASE_PATH / "data/raw/acs5_race_by_zcta5.csv", null_values="NA")
+        .drop("GEOID", "moe")
+        .rename({"NAME": "zcta5"})
+        .with_columns(pl.col("zcta5").str.replace_all("ZCTA5 ", "").cast(pl.Int64))
+        .collect()
+        .pivot(values="estimate", index=["zcta5", "year"], columns="variable")
+        .select(cs.all().name.map(lambda c: c.replace("B03002_", "")))
+        .rename(
+            {
+                "001": "total",
+                "002": "hispanic",
+                "003": "nh_white",
+                "004": "nh_black",
+                "005": "nh_aian",
+                "006": "nh_asian",
+                "007": "nh_hpi",
+                "008": "nh_other",
+                "009": "nh_multiracial",
+            }
+        )
+        .to_pandas()
+    )
+
+    # Cast to pandas because we want their .add() behavior, which is to treat nulls as
+    # 0 when there is another non-null value but to return null when all are null.
+    # Polars does not have this option.
+
+    df["nh_hpi"] = df["nh_asian"] + df["nh_hpi"]
+    df = df[
+        [
+            "zcta5",
+            "year",
+            "total",
+            "hispanic",
+            "nh_white",
+            "nh_black",
+            "nh_aian",
+            "nh_asian",
+            "nh_hpi",
+            "nh_other",
+            "nh_multiracial",
+        ]
+    ]
+
+    df.to_parquet(BASE_PATH / "data/race_by_zcta5.parquet", index=False)
+
+
+if __name__ == "__main__":
+    main()
